f800157f28f6b419834d4f550a75ab99940c9413
adarsh4505 <adarsh9628281469@gmail.com>
2022-07-25 14:56:50 +0530

    Second commit

---
+++ My First Java Project/src/javaBasics/helloworld.java
@@ -0,0 +1,10 @@
+package javaBasics;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
---
+++ java project/src/sum/Sum.java
@@ -0,0 +1,13 @@
+package sum;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 10;
+		int c = a + b;
+		System.out.println(c);
+	}
+
+}
